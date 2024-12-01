fd674ff3baae5d106b71d45f909afeb15cb9bad2
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2024-12-01 16:50:16 +0100

    Update ModConfig.java

--- src/main/java/com/noslimes/orebuff/config/ModConfig.java
+++ src/main/java/com/noslimes/orebuff/config/ModConfig.java
@@ -13,41 +13,41 @@ public class ModConfig {
 
     public ModConfig() {
         // Set default values
-        setOreConfig(Blocks.COAL_ORE.getLootTableId(), Items.COAL.toString(), true, 0.5f, 1, 2);
-        setOreConfig(Blocks.DEEPSLATE_COAL_ORE.getLootTableId(), Items.COAL.toString(), true, 0.5f, 1, 2);
+        setOreConfig(Blocks.COAL_ORE.getLootTableId(), Items.COAL.toString(), true, false, 0.5f, 2);
+        setOreConfig(Blocks.DEEPSLATE_COAL_ORE.getLootTableId(), Items.COAL.toString(), true, false, 0.5f, 2);
 
-        setOreConfig(Blocks.IRON_ORE.getLootTableId(), Items.RAW_IRON.toString(), true, 0.35f, 1, 1);
-        setOreConfig(Blocks.DEEPSLATE_IRON_ORE.getLootTableId(), Items.RAW_IRON.toString(), true, 0.35f, 1, 1);
+        setOreConfig(Blocks.IRON_ORE.getLootTableId(), Items.RAW_IRON.toString(), true, false, 0.35f, 1);
+        setOreConfig(Blocks.DEEPSLATE_IRON_ORE.getLootTableId(), Items.RAW_IRON.toString(), true, false, 0.35f, 1);
 
-        setOreConfig(Blocks.COPPER_ORE.getLootTableId(), Items.RAW_COPPER.toString(), false, 0.35f, 1, 2);
-        setOreConfig(Blocks.DEEPSLATE_COPPER_ORE.getLootTableId(), Items.RAW_COPPER.toString(), false, 0.35f, 1, 2);
+        setOreConfig(Blocks.COPPER_ORE.getLootTableId(), Items.RAW_COPPER.toString(), false, false, 0.35f, 2);
+        setOreConfig(Blocks.DEEPSLATE_COPPER_ORE.getLootTableId(), Items.RAW_COPPER.toString(), false, false, 0.35f, 2);
 
-        setOreConfig(Blocks.GOLD_ORE.getLootTableId(), Items.RAW_GOLD.toString(), true, 0.35f, 1, 1);
-        setOreConfig(Blocks.DEEPSLATE_GOLD_ORE.getLootTableId(), Items.RAW_GOLD.toString(), true, 0.35f, 1, 1);
+        setOreConfig(Blocks.GOLD_ORE.getLootTableId(), Items.RAW_GOLD.toString(), true, false, 0.35f, 1);
+        setOreConfig(Blocks.DEEPSLATE_GOLD_ORE.getLootTableId(), Items.RAW_GOLD.toString(), true, false, 0.35f, 1);
 
-        setOreConfig(Blocks.LAPIS_ORE.getLootTableId(), Items.LAPIS_LAZULI.toString(), false, 0.35f, 1, 2);
-        setOreConfig(Blocks.DEEPSLATE_LAPIS_ORE.getLootTableId(), Items.LAPIS_LAZULI.toString(), false, 0.35f, 1, 2);
+        setOreConfig(Blocks.LAPIS_ORE.getLootTableId(), Items.LAPIS_LAZULI.toString(), false, false, 0.35f, 2);
+        setOreConfig(Blocks.DEEPSLATE_LAPIS_ORE.getLootTableId(), Items.LAPIS_LAZULI.toString(), false, false, 0.35f, 2);
 
-        setOreConfig(Blocks.REDSTONE_ORE.getLootTableId(), Items.REDSTONE.toString(), false, 0.35f, 1, 3);
-        setOreConfig(Blocks.DEEPSLATE_REDSTONE_ORE.getLootTableId(), Items.REDSTONE.toString(), false, 0.35f, 1, 3);
+        setOreConfig(Blocks.REDSTONE_ORE.getLootTableId(), Items.REDSTONE.toString(), false, false, 0.35f, 3);
+        setOreConfig(Blocks.DEEPSLATE_REDSTONE_ORE.getLootTableId(), Items.REDSTONE.toString(), false, false, 0.35f, 3);
 
-        setOreConfig(Blocks.EMERALD_ORE.getLootTableId(), Items.EMERALD.toString(), false, 0.15f, 1, 1);
-        setOreConfig(Blocks.DEEPSLATE_EMERALD_ORE.getLootTableId(), Items.EMERALD.toString(), false, 0.15f, 1, 1);
+        setOreConfig(Blocks.EMERALD_ORE.getLootTableId(), Items.EMERALD.toString(), false, false, 0.15f, 1);
+        setOreConfig(Blocks.DEEPSLATE_EMERALD_ORE.getLootTableId(), Items.EMERALD.toString(), false, false, 0.15f, 1);
 
-        setOreConfig(Blocks.DIAMOND_ORE.getLootTableId(), Items.DIAMOND.toString(), false, 0.15f, 1, 1);
-        setOreConfig(Blocks.DEEPSLATE_DIAMOND_ORE.getLootTableId(), Items.DIAMOND.toString(), false, 0.15f, 1, 1);
+        setOreConfig(Blocks.DIAMOND_ORE.getLootTableId(), Items.DIAMOND.toString(), false, false, 0.15f, 1);
+        setOreConfig(Blocks.DEEPSLATE_DIAMOND_ORE.getLootTableId(), Items.DIAMOND.toString(), false, false, 0.15f, 1);
 
-        setOreConfig(Blocks.NETHER_QUARTZ_ORE.getLootTableId(), Items.QUARTZ.toString(), true,0.35f, 1, 2);
+        setOreConfig(Blocks.NETHER_QUARTZ_ORE.getLootTableId(), Items.QUARTZ.toString(), true, false,0.35f, 2);
 
-        setOreConfig(Blocks.ANCIENT_DEBRIS.getLootTableId(), Items.NETHERITE_SCRAP.toString(), false, 0.05f, 1, 1);
+        setOreConfig(Blocks.ANCIENT_DEBRIS.getLootTableId(), Items.NETHERITE_SCRAP.toString(), false, false, 0.05f, 1);
     }
 
     public Map<Identifier, OreConfig> getOreConfigs(){
         return oreConfigs;
     }
 
-    private void setOreConfig(Identifier oreName, String itemName, boolean enable, float chance, int minCount, int maxCount) {
-        oreConfigs.put(oreName, new OreConfig(itemName, enable, chance, minCount, maxCount));
+    private void setOreConfig(Identifier oreName, String itemName, boolean enable, boolean replace, float chance, int maxCount) {
+        oreConfigs.put(oreName, new OreConfig(itemName, enable, replace, chance, 1, maxCount));
     }
 
     public static class OreConfig {
